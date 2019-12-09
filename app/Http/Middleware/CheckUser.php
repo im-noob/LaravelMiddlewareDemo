935225f1c2c1d7b5410d65b5e00393a34bfb2c2d
amriteshonly4u <amriteshonly4u@gmail.com>
2019-12-10 02:23:44 +0530

    init

---
+++ app/Http/Middleware/CheckUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->is_admin() == 0) {
+            return $next($request);
+            // return redirect('userList');
+        }else{
+            return redirect('login');
+        }
+        
+    }
+}
